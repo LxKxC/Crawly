b25afe09a3b2ba2110e93120433740a98b5828a1
ZenixIs <26434745+ZenixIs@users.noreply.github.com>
2018-06-06 00:37:29 +0200

    New method

--- crawly/core/http.py
+++ crawly/core/http.py
@@ -38,13 +38,30 @@ class URI:
 			self._port = port
 		
 	def port(self):
+		# returns the port of the website
 		self.parsing()
 		return self._port
 
 	def host(self):
+		# returns the domain
 		self.parsing()
 		return self._host
-
+	
+	def path(self):
+		# return only gived paths
+		# url = "http://test.com/test/adm"
+		# result = "/test/adm"
+		if "http://" in self.URL:
+			self.URL = self.URL.replace("http://", "")
+		
+		elif "https://" in self.URL:
+			self.https = True
+			self.URL = self.URL.replace("https://", "")
+		
+		path = self.URL.split("/")[1:]
+		
+		return  "/" + "/".join(path)
+		
 	def prepare(self):
 		# Prepare the URL, ready for
 		# bruteforcing.
