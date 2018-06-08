356a2b4ab8ebae4b9e829f59896f89b4ff1007b7
ZenixIs <26434745+ZenixIs@users.noreply.github.com>
2018-06-08 23:31:31 +0200

    imports added

--- crawly/__init__.py
+++ crawly/__init__.py
@@ -1,1 +1,24 @@
-pass
+# < Core files >
+# Importing error classes
+from core.errors import WindowsError
+from core.errors import BadURLError
+# Importing color strings
+from core.headers import Strings
+# Importing http URI module
+from core.http import URI
+# Importing CLI stdout module
+from core.stdout import CLI
+# Importing tool module
+from core.tool import Tools
+#
+# < Lib files >
+# Scan module
+from lib.scan import Crawl
+from lib.scan import DirBrute
+from lib.scan import DNSBrute
+# Attack module
+from lib.attack import Shellshock
+from lib.attack import Bashdoor
+from lib.attack import HTMLBrute
+from lib.attack import HTTPBrute
+from lib.attack import SSHBrute
