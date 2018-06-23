fe4b84b25da919558cbf9db0351a849ee2ab2355
ZenixIs <26434745+ZenixIs@users.noreply.github.com>
2018-06-23 23:44:22 +0200

    moving doc

--- install.bat
+++ install.bat
@@ -1,5 +1,8 @@
 copy "README.md" "C:\Program Files\Crawly\"
 md "C:\Program Files\Crawly\db\"
+md "C:\Program Files\Crawly\doc\"
 cd "crawly\db\"
 copy * "C:\Program Files\Crawly\db\"
+cd "..\..\doc\"
+copy * "C:\Program Files\Crawly\doc\"
 exit 0
